77ff016cfc1a3a87e8dff73eb6aeebade6a6462b
kylehuang041 <kylehuang041@gmail.com>
2022-07-28 20:52:39 -0700

    created Sudoku.cpp and Sudoku.h

---
+++ Sudoku.h
@@ -0,0 +1,14 @@
+/**
+ * @file Sudoku.h
+ * @author Qihou Zhang, Cuc Doan, Kyle Huang
+ * @brief Sudoku Puzzle Solver
+ * @version 0.1
+ * @date 2022-07-28
+ * @copyright Copyright (c) 2022
+ */
+
+#include <iostream>
+
+class Sudoku {
+
+};
