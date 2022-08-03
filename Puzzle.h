eacd18d093faa9a6041e4a4fad301aa9761b76ee
kylehuang041 <kylehuang041@gmail.com>
2022-08-02 21:31:32 -0700

    new files and updated driver

---
+++ Puzzle.h
@@ -0,0 +1,22 @@
+/**
+ * @file Puzzel.h
+ * @author Qihou Zhang, Cuc Doan, Kyle Huang
+ * @brief 
+ * @version 0.1
+ * @date 2022-08-02
+ * @copyright Copyright (c) 2022
+ */
+
+#pragma once
+#ifndef PUZZLE_H
+#define PUZZLE_H
+
+class Puzzle {
+
+    public:
+        Puzzle() {
+
+        }
+};
+
+#endif
