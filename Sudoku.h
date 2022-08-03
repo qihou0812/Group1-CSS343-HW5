d2bd9176d4f52c510e0a0464124561b358c2e729
kylehuang041 <kylehuang041@gmail.com>
2022-08-02 20:45:45 -0700

    command line arguments

--- Sudoku.h
+++ Sudoku.h
@@ -7,8 +7,22 @@
  * @copyright Copyright (c) 2022
  */
 
+#pragma once
+#ifndef SUDOKU_H
+#define SUDOKU_H
+
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 class Sudoku {
+    int population, generation;
 
+    public:
+        Sudoku(int population, int generation)
+            : population(population), generation(generation) {
+                srand(time(NULL));
+            }
 };
+
+#endif
