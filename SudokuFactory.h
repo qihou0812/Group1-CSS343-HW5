f96f98e04616320d3d7b55341f16b024a9684304
kylehuang041 <kylehuang041@gmail.com>
2022-08-07 23:12:12 -0700

    added header guard

--- SudokuFactory.h
+++ SudokuFactory.h
@@ -1,5 +1,17 @@
+/**
+ * @file SudokuFactory.h
+ * @author Qihou Zhang, Cuc Doan, Kyle Huang
+ * @brief 
+ * @version 0.1
+ * @date 2022-08-07
+ * @copyright Copyright (c) 2022
+ */
+
+#pragma once
+#ifndef SUDOKUFACTORY_H
+#define SUDOKUFACTORY_H
+
 #include <iostream>
-#include "Puzzle.h"
 #include "PuzzleFactory.h"
 
 class SudokuFactory : public PuzzleFactory {
@@ -7,3 +19,5 @@ class SudokuFactory : public PuzzleFactory {
     public:
         SudokuFactory();
 };
+
+#endif
